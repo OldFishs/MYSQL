7c1347a147bee672ede13bd245dab600e6820bae
OldFishs <3205710366@qq.com>
2024-09-06 15:50:47 +0800

    更新 database2.sql

---
+++ database2.sql
@@ -0,0 +1,31 @@
+#演示删除和查询数据库
+#查看当前数据库服务器中的所有数据库
+SHOW DATABASES
+#查看前面创建的xxx_db01数据库的定义信息
+SHOW CREATE DATABASE xxx_db01
+#创建数据库时为规避关键字，可使用反引号
+CREATE DATABASE `create`
+#删除前面创建的xxx_db01数据库
+DROP DATABASE xxx_db01
+
+#练习homework1备份xxx_0203信息并恢复
+
+#备份（dos 下执行mysqldump在bin\目录下
+mysqldump -u root -p -B xxx_db02 xxx_db03 > d:\\bak.sql
+
+DROP DATABASE xxx_db02
+`test1`
+
+#恢复
+SOURCE 文件名.sql
+
+#指令创建表
+CREATE TABLE `user`(
+	id INT,
+	`name` VARCHAR(255),
+	`password` VARCHAR(255),
+	`birthday` DATE)
+CREATE TABLE t3(
+	id TINYINT)
+	
+INSERT INTO `t3` VALUE (127);
